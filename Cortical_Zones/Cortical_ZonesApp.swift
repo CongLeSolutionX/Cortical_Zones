00cfdf36a1723096f67405f80d57165ccd4b80a6
Cong Le <longchike@gmail.com>
2025-06-29 00:17:10 -0700

    The setup

---
+++ Cortical_Zones/Cortical_ZonesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cortical_ZonesApp.swift
+//  Cortical_Zones
+//
+//  Created by Cong Le on 6/29/25.
+//
+
+import SwiftUI
+
+@main
+struct Cortical_ZonesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
